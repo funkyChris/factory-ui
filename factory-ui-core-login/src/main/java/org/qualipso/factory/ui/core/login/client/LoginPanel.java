cfc2428d7284c42bd2a6c065795751417fb7e89d
Christophe Bouthier <Christophe.Bouthier@loria.fr>
2009-11-23 11:28:23 +0100

    Using UiHandler instead of ClickHandler

--- factory-ui-core-login/src/main/java/org/qualipso/factory/ui/core/login/client/LoginPanel.java
+++ factory-ui-core-login/src/main/java/org/qualipso/factory/ui/core/login/client/LoginPanel.java
@@ -22,9 +22,9 @@ package org.qualipso.factory.ui.core.login.client;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
@@ -38,7 +38,7 @@ import com.google.gwt.user.client.ui.VerticalPanel;
  * @author <a href="mailto:christophe.bouthier@loria.fr">Christophe Bouthier</a>
  * @date 10 November 2009
  */
-public class LoginPanel extends Composite implements ClickHandler {
+public class LoginPanel extends Composite {
     
     interface MyUiBinder extends UiBinder<VerticalPanel, LoginPanel> {}
     private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
@@ -53,10 +53,9 @@ public class LoginPanel extends Composite implements ClickHandler {
     public LoginPanel(Login loginManager) {
         initWidget(uiBinder.createAndBindUi(this));
         this.loginManager = loginManager;
-        loginButton.addClickHandler(this);
     }
 
-    @Override
+    @UiHandler("loginButton")
     public void onClick(ClickEvent arg0) {
         loginManager.login(usernameTextBox.getText(), passwordTextBox.getText());
     }
